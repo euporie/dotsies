0c299546b97f31128b68d7b9320964baa8e11e58
Kuba Niewiarowski <marsjaninzmarsa@gmail.com>
2014-02-07 03:07:56 +0100

    some blank shots to fix

--- keyboard.ino
+++ keyboard.ino
@@ -1,5 +1,5 @@
 int buttonPins[5] = {5,6,7,8,9};  // Set a button to any pin
-char charMap[28] = {
+char charMap[26] = {
   'a',
   'b',
   'c',
@@ -44,7 +44,7 @@ void loop()
   int i;
   do {
     read = 0;
-    for (i = 0; i < 5; i++) {
+    for (i = 4; i >= 0; i--) {
       read = (read << 1);
       if (digitalRead(buttonPins[i]) == 0) { // if the button goes low
         delay(10);
